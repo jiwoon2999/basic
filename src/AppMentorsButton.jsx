2448061354da7d2a5ccf0bde58a632f1be476e10
jiwoon2999 <jiwoon2999@naver.com>
2023-03-05 15:20:59 +0900

    로딩에러처리 공부

--- src/AppMentorsButton.jsx
+++ src/AppMentorsButton.jsx
@@ -1,23 +1,23 @@
-import React, { useReducer } from 'react';
+import React, { memo, useCallback, useMemo, useReducer } from 'react';
 import personReducer from './reducer/person-reducer';
 
-export default function AppMentors() {
+export default function AppMentorsButton() {
   const [person, dispatch] = useReducer(personReducer, initialPerson);
-  const handleUpdate = () => {
+  const handleUpdate = useCallback(() => {
     const prev = prompt('누구의 이름을 바꾸고 싶은가요?');
     const current = prompt('이름을 무엇으로 바꾸고 싶은가요?');
     dispatch({ type: 'updated', prev, current });
-  };
+  }, []);
 
-  const handleAdd = () => {
+  const handleAdd = useCallback(() => {
     const name = prompt('새로운 멘토는 누구입니까?');
     const title = prompt('직책은 무엇입니까?');
     dispatch({ type: 'added', name, title });
-  };
-  const handleDelete = () => {
+  }, []);
+  const handleDelete = useCallback(() => {
     const name = prompt('누구의 이름을 삭제하고 싶습니까?');
     dispatch({ tpye: 'deleted', name });
-  };
+  }, []);
 
   return (
     <div>
@@ -30,11 +30,28 @@ export default function AppMentors() {
           </li>
         ))}
       </ul>
-      <button onClick={handleUpdate}>멘토의 이름을 바꾸기</button>
-      <button onClick={handleAdd}>멘토 추가하기</button>
-      <button onClick={handleDelete}>멘토 삭제하기</button>
+      <Button text='멘토의 이름을 바꾸기' onClick={handleUpdate} />
+      <Button text='추가하기' onClick={handleAdd} />
+      <Button text='삭제하기' onClick={handleDelete} />
     </div>
   );
+}
+
+const Button = memo(({ text, onClick }) => {
+  console.log('Button', text, 're-rendering');
+  const result = useMemo(() => calculateSomething(), []);
+  return (
+    <button onClick={onClick} style={{ backgroundColor: 'black', color: 'white', borderRadius: '20px', margin: '0.4rem' }}>
+      {`${text} ${result}`}
+    </button>
+  );
+});
+
+function calculateSomething() {
+  for (let i = 0; i < 10000; i++) {
+    console.log('haha');
+  }
+  return 10;
 }
 
 const initialPerson = {
--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react';
 
 export default function Products() {
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('데이터를 받아왔는가?');
         setProducts(data);
-      });
+      })
+      .catch((e) => setError('에러발생'))
+      .finally(() => setLoading(false));
     return () => {
       console.log('청소해라');
     };
   }, [checked]);
-
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <>
       <input id='checkbox' type='checkbox' value={checked} onChange={handleChange} />
